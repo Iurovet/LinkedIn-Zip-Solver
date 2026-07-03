8c71777b07eccc86943391a28f178a9e4aa79b15
Iurovet <s4003879@student.rmit.edu.au>
2026-07-03 16:22:39 +1000

    Add remove logic such that subsequent cells reduce in numbering

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Run at the start
 document.addEventListener('DOMContentLoaded', () => {
-    setDimensions(6);
-    initialiseCells();
+    setDimensions(6); // Default dimensions 6x6
+    initialiseCells(); // Remove all cells and set their behaviour
 });
 
 
-let addMode = true;
+let editMode = true;
 let nextNum = 1;
 
 function initialiseCells() {
@@ -24,20 +24,30 @@ function initialiseCells() {
       // Click functionality
       columns[j].addEventListener('click', function() {
         // Show the circle and increment the next number
-        if (addMode && !(this.classList.contains('numberable'))) {
+        if (editMode && !this.classList.contains('numberable')) {
           this.classList.add('numberable');
           this.textContent = String(nextNum);
           nextNum++;
         }
 
         // Hide the circle and decrement the number
-        else if (!addMode && this.classList.contains('numberable')) {
+        else if (!editMode && this.classList.contains('numberable')) {
           this.classList.remove('numberable');
+
+          let currNumber = parseInt(this.textContent);
+          this.textContent = ""; // Remove the current number
+
+          updateNumbers(currNumber); // All higher elements get reduced by one
           nextNum--;
         }
       });
     }
   }
+}
+
+// Set cells to either add (true) or remove (false)
+function setEditMode(value){
+  editMode = value;
 }
 
 // Sets table size to a square of side length 5, 6, 7 or 8
@@ -56,4 +66,30 @@ function setDimensions(size) {
       columns[j].style.display = (j >= size) ? 'none' : '';
     }
   }
+}
+
+function updateNumbers(currNum) {
+  /* Find where a cell is filled in and sort it by number (assuming it
+   * exists, hence why the array must be flattened first). Using numeric
+   * ensures behaviour of the type [1, 2, 10], rather than [1, 10, 2] in terms
+   * of what textContent is equal to.
+  */
+  
+  // Find the table
+  const table = document.querySelector('table').tBodies[0];
+  const filledCells = [...table.rows].map(row => [...row.cells])
+  .flat().filter(el => 
+    el.textContent !== "" && el.classList.contains('numberable')
+  ).sort((a, b) => 
+    a.textContent.localeCompare(b.textContent, undefined, { numeric: true })
+  );
+
+  /* There should be a gap of 1 precisely where an element was removed.
+   * Any cell above that number should be decremented by 1 to fill said gap.
+  */
+  filledCells.forEach(element => {
+    if (parseInt(element.textContent) > currNum) {
+      element.textContent = (parseInt(element.textContent, 10) - 1).toString();
+    }
+  })
 }
