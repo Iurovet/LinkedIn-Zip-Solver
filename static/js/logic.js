fa15e607f86bdb3b5e7015b4ab200c0e97b94431
Iurovet <s4003879@student.rmit.edu.au>
2026-07-03 15:34:22 +1000

    Add in functionality to add cells (might not be the best UI)

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,48 @@
 // Run at the start
 document.addEventListener('DOMContentLoaded', () => {
     setDimensions(6);
+    initialiseCells();
 });
+
+
+let addMode = true;
+let nextNum = 1;
+
+function initialiseCells() {
+  // Find the table
+  const table = document.querySelector('table').tBodies[0];
+  const rows = table.rows;
+  
+  // Look for individual elements
+  for (let i = 0; i < rows.length; i++) {
+    const columns = rows[i].cells;
+
+    for (let j = 0; j < columns.length; j++) {
+      // Remove everything from the start
+      columns[j].classList.remove('numberable');
+
+      // Click functionality
+      columns[j].addEventListener('click', function() {
+        // Show the circle and increment the next number
+        if (addMode && !(this.classList.contains('numberable'))) {
+          this.classList.add('numberable');
+          this.textContent = String(nextNum);
+          nextNum++;
+        }
+
+        // Hide the circle and decrement the number
+        else if (!addMode && this.classList.contains('numberable')) {
+          this.classList.remove('numberable');
+          nextNum--;
+        }
+      });
+    }
+  }
+}
 
 // Sets table size to a square of side length 5, 6, 7 or 8
 function setDimensions(size) {
-  // Find the table and rows
+  // Find the table
   const table = document.querySelector('table').tBodies[0];
   const rows = table.rows;
   
